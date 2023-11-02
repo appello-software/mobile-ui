4d908507a140bd7c80fa42a4cdb4e118d1d03ddb
kuzkokov <kuzkokov@gmail.com>
2023-11-02 16:23:52 +0300

    MUK001: Improve BasicHeader displaying

--- src/components/screens/BasicHeader.tsx
+++ src/components/screens/BasicHeader.tsx
@@ -2,6 +2,7 @@ import { HeaderBackContext } from '@react-navigation/elements';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, TouchableOpacity, View, ViewStyle } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { SvgProps } from 'react-native-svg';
 
 import { AppText } from '~/components/common/AppText';
@@ -13,6 +14,8 @@ import { makeStyles } from '~/utils';
 export interface BasicHeaderProps {
   /* Icon of back button */
   BackButtonIcon?: React.FC<SvgProps>;
+  /* Should hide back button even if it's possible to go back */
+  hideBackButton?: boolean;
   /* Title to display in the middle of header */
   title?: string;
   /* Any element to display in the right part of header */
@@ -37,7 +40,7 @@ export const BasicHeader: React.FC<BasicHeaderProps> = props => {
   const navigation = useNavigation();
   const route = useRoute();
 
-  const { BackButtonIcon, accessoryRight, title } = useCombinedPropsWithConfig(
+  const { BackButtonIcon, hideBackButton, accessoryRight, title } = useCombinedPropsWithConfig(
     'BasicHeader',
     props,
   );
@@ -49,27 +52,33 @@ export const BasicHeader: React.FC<BasicHeaderProps> = props => {
   const backButton = (
     <HeaderBackContext.Consumer>
       {headerBack => {
-        if (headerBack && BackButtonIcon) {
+        if (headerBack && BackButtonIcon && !hideBackButton) {
           return (
-            <TouchableOpacity activeOpacity={0.75} onPress={onBackPress}>
+            <TouchableOpacity
+              activeOpacity={0.75}
+              onPress={onBackPress}
+              style={innerStyles['basic-button__left-accessory']}
+            >
               <BackButtonIcon color={colors.black[1]} width={24} height={24} />
             </TouchableOpacity>
           );
         }
 
-        return <View />;
+        return <View style={innerStyles['basic-button__left-accessory']} />;
       }}
     </HeaderBackContext.Consumer>
   );
 
   return (
-    <View style={styles['basic-header']}>
-      {backButton}
-      <AppText variant="p3" color={colors.black['1']}>
-        {title || route.name}
-      </AppText>
-      <View style={innerStyles['basic-header__right-accessory']}>{accessoryRight}</View>
-    </View>
+    <SafeAreaView edges={['top']}>
+      <View style={styles['basic-header']}>
+        {backButton}
+        <AppText variant="p3" color={colors.black['1']}>
+          {title || route.name}
+        </AppText>
+        <View style={innerStyles['basic-header__right-accessory']}>{accessoryRight}</View>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -85,8 +94,16 @@ export const useBasicHeaderStyles = makeStyles<void, BasicHeaderStyles>(({ shado
 }));
 
 const innerStyles = StyleSheet.create({
+  'basic-button__left-accessory': {
+    alignSelf: 'stretch',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    width: '20%',
+  },
   'basic-header__right-accessory': {
+    alignSelf: 'stretch',
     alignItems: 'flex-end',
     justifyContent: 'center',
+    width: '20%',
   },
 });
