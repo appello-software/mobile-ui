5c2e7628348bff8788c8129ff4f428ccb2cd0758
kuzkokov <kuzkokov@gmail.com>
2024-02-28 21:41:41 +0300

    MUK002: Implement builds with bob + add AsyncStorage to the storybook

--- .storybook/index.ts
+++ .storybook/index.ts
@@ -1,5 +1,13 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import { view } from './configs/ondevice/storybook.requires';
 
-export const StorybookUIRoot = view.getStorybookUI({
+const StorybookUIRoot = view.getStorybookUI({
   shouldDisableKeyboardAvoidingView: true,
+  storage: {
+    getItem: AsyncStorage.getItem,
+    setItem: AsyncStorage.setItem,
+  },
 });
+
+export default StorybookUIRoot;
